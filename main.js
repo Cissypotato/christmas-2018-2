3ecd1ec9d3cf2542f178c7ae342e44b316dc8abc
Cissy <1395422644@qq.com>
2018-12-25 18:53:27 +0800

    2

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 !function(){
-
-  
+  var play=document.querySelector("#play")
+  var pause=document.querySelector("#pause")
+  var music=document.querySelector("#music")
+  play.addEventListener('click',function(e){
+    e.currentTarget.classList.add('active')
+    pause.classList.remove('active')
+    music.pause()
+  })
+  pause.addEventListener('click',function(e){
+    e.currentTarget.classList.add('active')
+    play.classList.remove('active')
+    music.play()
+  })
 
 }()
